cbb647cb37cba1ff5041fa2521d85f41b2654983
大喜多真萌 <mahookita@MahoMacBook-Pro.local>
2024-07-16 15:45:15 +0900

    写真の追加

--- app/order/components/cartContent/CartContent.tsx
+++ app/order/components/cartContent/CartContent.tsx
@@ -19,13 +19,15 @@ const CartContent: React.FC<CartContentProps> = ({ items }) => {
   return (
     <div className={styled.cart}>
       <h2>注文内容</h2>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            {item.name} - {item.quantity}個 - ¥{item.price}
-          </li>
-        ))}
-      </ul>
+      <div className={styled.cart_detail}>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.name} - {item.quantity}個 - ¥{item.price}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
